22bedc1efcde6f244ee59fcd3f520304719cb1bc
kkauut@gmail.com <kkauut@gmail.com>
2010-10-24 05:15:57 +0000

    проверка

--- SAMA.java
+++ SAMA.java
@@ -27,58 +27,66 @@ class Pawn {
 		return left+up+right;
 	}
 }
+
 class Move
 {
 	private int X=3;
 	private int Y=3;
 	private boolean gameOver = false;
-	private int[][] busy;
+	private int[][] gameBoard;
 	Pawn[] pawns;
-	public void setGameOver( boolean gameOver) {
-		this.gameOver = gameOver;
+	public void setGameOver() {
+		
 	}
-	public boolean getGameOver( ) {
+	public boolean getGameOver() {
 		return gameOver;
 	}
-	public void setBusy() {
-		busy = new int [Y][X];
+	public void setGameBoard() {
+		gameBoard = new int [Y][X];
 	}
-	public void setBusy(String []a) {
+	public void setGameBoard(String []a) {
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++)
-				busy[i][j] = Integer.valueOf(a[i*X + j]);
+				gameBoard[i][j] = Integer.valueOf(a[i*X + j]);
 	}
-	public void setBusy(int []a) {
+	public void setGameBoard(int []a) {
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++)
-				busy[i][j] = a[i*X + j];
+				gameBoard[i][j] = a[i*X + j];
 	}
-	public void setBusy(int y, int x, int p) {
-				busy[y][x] = p;
+	public void setGameBoard(int y, int x, int p) {
+				gameBoard[y][x] = p;
 	}
-	public void setNewBusy() {
-		busy = new int [Y][X];
+	public void setNewGameBoard() {
+		gameBoard = new int [Y][X];
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++) {
 				if(i == 0)
-					busy[i][j] = 2;
+					gameBoard[i][j] = 2;
 				else if(i == Y-1)
-					busy[i][j] = 1;
+					gameBoard[i][j] = 1;
 				else
-					busy[i][j] = 0;
+					gameBoard[i][j] = 0;
 			}
 	}
-	public int getBusy(int y, int x) {
-		return busy[y][x];
+	public int getGameBoard(int y, int x) {
+		return gameBoard[y][x];
 	}
-	public int[][] getBusy() {
-		return busy;
+	public void getGameBoardInSystem() {
+		for(int i = 0; i < Y; i++) {
+			for(int j = 0; j < X; j++)
+				System.out.print(gameBoard[i][j]+" ");
+			System.out.println();
+		}
+	}
+	public int[][] getGameBoard() {
+		return gameBoard;
 	}
 	public int getPawn() {
 		int pawn=0;
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++)
-				if(busy[i][j] == 2)
+				if(gameBoard[i][j] == 2)
 					pawn++;
 		return pawn;
 	}
@@ -88,40 +96,20 @@ class Move
 	public int getY() {
 		return Y;
 	}
-	public int getPawnX(int numberOfPawns) {
+	public int getPawnX(int numberOfPawns, int player) {
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++)
-				if(busy[i][j] == 2) {
+				if(gameBoard[i][j] == player) {
 					numberOfPawns--;
 					if(numberOfPawns == 0)
 						return j;
 				}
 		return -1;
 	}
-	public int getPawnY(int numberOfPawns) {
+	public int getPawnY(int numberOfPawns, int player) {
 		for(int i = 0; i < Y; i++)
 			for(int j = 0; j < X; j++)
-				if(busy[i][j] == 2) {
-					numberOfPawns--;
-					if(numberOfPawns == 0)
-						return i;
-				}
-		return -1;
-	}
-	public int getPPawnX(int numberOfPawns) {
-		for(int i = 0; i < Y; i++)
-			for(int j = 0; j < X; j++)
-				if(busy[i][j] == 1) {
-					numberOfPawns--;
-					if(numberOfPawns == 0)
-						return j;
-				}
-		return -1;
-	}
-	public int getPPawnY(int numberOfPawns) {
-		for(int i = 0; i < Y; i++)
-			for(int j = 0; j < X; j++)
-				if(busy[i][j] == 1) {
+				if(gameBoard[i][j] == player) {
 					numberOfPawns--;
 					if(numberOfPawns == 0)
 						return i;
@@ -144,8 +132,8 @@ class Data
 			moves[i] = new Move();
 			String str2 = br.readLine();
 			String a[] = str2.split("\t");
-			moves[i].setBusy();
-			moves[i].setBusy(a);
+			moves[i].setGameBoard();
+			moves[i].setGameBoard(a);
 			pawn=moves[i].getPawn();
 			moves[i].pawns = new Pawn[pawn];
 			for(int l = 0; l < pawn; l++) {
@@ -165,7 +153,7 @@ class Data
 		for (int i = 0; i < move; i++) {
 			for (int j = 0; j < moves[i].getY(); j++)
 				for (int l = 0; l < moves[i].getX(); l++)
-					pw.print(moves[i].getBusy(j, l) + "\t");
+					pw.print(moves[i].getGameBoard(j, l) + "\t");
 			pw.print("\n");
 			for (int j = 0; j < pawn; j++) {
 				pw.print(moves[i].pawns[j].getLeft() + "\t");
@@ -182,7 +170,7 @@ class Data
 		for (int i = 0; i < move; i++) {
 			for (int j = 0; j < moves[i].getY(); j++)
 				for (int l = 0; l < moves[i].getX(); l++)
-					pw.print(moves[i].getBusy(j, l) + "\t");
+					pw.print(moves[i].getGameBoard(j, l) + "\t");
 			pw.print("\n");
 			for (int j = 0; j < pawn; j++) {
 				pw.print(moves[i].pawns[j].getLeft() + "\t");
@@ -192,7 +180,7 @@ class Data
 		}
 		for (int j = 0; j < newMove.getY(); j++)
 			for (int l = 0; l < newMove.getX(); l++)
-				pw.print(newMove.getBusy(j, l) + "\t");
+				pw.print(newMove.getGameBoard(j, l) + "\t");
 		pw.print("\n");
 		for (int j = 0; j < pawn; j++) {
 			pw.print(newMove.pawns[j].getLeft() + "\t");
@@ -226,15 +214,14 @@ class SAMA {
 	}
 	public static void game() throws IOException {
 		Move move = new Move();
-		move.setNewBusy();
-		boardInSystem(move);
+		move.setNewGameBoard();
+		move.getGameBoardInSystem();
 		while(!move.getGameOver()) {
 			move = PMove(move);
-			boardInSystem(move);
+			move.getGameBoardInSystem();
 			if(move.getGameOver())
 				return;
 			move = SMove(move);
-			boardInSystem(move);
 		}
 		saveSMove();
 	}
@@ -243,15 +230,15 @@ class SAMA {
 		String choice=br.readLine();
 		String a[] = choice.split(" ");
 		int numberOfPawns = Integer.valueOf(a[0]);
-		int x = move.getPPawnX(numberOfPawns);
-		int y = move.getPPawnY(numberOfPawns);
-		move.setBusy(y, x, 0);
+		int x = move.getPawnX(numberOfPawns, 1);
+		int y = move.getPawnY(numberOfPawns, 1);
+		move.setGameBoard(y, x, 0);
 		if(a[1].equals(UP))
-			move.setBusy(y-1, x, 1);
+			move.setGameBoard(y-1, x, 1);
 		else if(a[1].equals(LEFT))
-			move.setBusy(y-1, x-1, 1);
+			move.setGameBoard(y-1, x-1, 1);
 		else
-			move.setBusy(y-1, x+1, 1);
+			move.setGameBoard(y-1, x+1, 1);
 		return move;
 	}
 	public static Move SMove(Move move) throws IOException {
@@ -259,13 +246,6 @@ class SAMA {
 	}
 	public static void saveSMove() throws IOException {
 
-	}
-	public static void boardInSystem(Move move) {
-		for(int i = 0; i < move.getY(); i++) {
-			for(int j = 0; j < move.getX(); j++)
-				System.out.print(move.getBusy(i, j)+" ");
-			System.out.println();
-		}
 	}
 	public static boolean sM(int a[], int b[])  {
 		if (a.length!=b.length)
